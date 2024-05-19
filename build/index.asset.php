9ee46777bad4681e47b4105f0a27675847fba906
Hasan Chowdhury <chowdhury.hms@gmail.com>
2024-05-19 19:04:29 +0600

    Display placeholder text for the pixel art drawing inside the block editor

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '28c2df495407bfb6d67a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '85d1cb15a4b5b8fb9331');
