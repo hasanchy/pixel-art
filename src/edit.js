84a1eba7ec312cc1bf327dd44aeccced8959036e
Hasan Chowdhury <chowdhury.hms@gmail.com>
2024-05-19 19:35:20 +0600

    Display placeholder text for the pixel art drawing inside the block editor

--- src/edit.js
+++ src/edit.js
@@ -34,36 +34,11 @@ import './editor.scss';
 export default function Edit(props) {
   const { attributes: { size }, setAttributes } = props;
 
-	const blockProps = useBlockProps({
-    className: 'pixel-art-block',
-    style: { fontSize: size + 'px', lineHeight: '1' }
-});
+	const blockProps = useBlockProps();
 
 const onChangeSize = (newSize) => {
     setAttributes({ size: newSize });
 };
-
-	const svg = (
-		<svg width={size} height={size} xmlns="http://www.w3.org/2000/svg">
-  <rect width="16" height="16" x="0" y="0" fill="red" />
-  <rect width="32" height="32" x="32" y="0" fill="gold" />
-  <rect width="32" height="32" x="64" y="0" fill="red" />
-  <rect width="32" height="32" x="96" y="0" fill="#000" />
-  <rect width="32" height="32" x="0" y="32" fill="orange" />
-  <rect width="32" height="32" x="32" y="32" fill="red" />
-  <rect width="32" height="32" x="64" y="32" fill="purple" />
-  <rect width="32" height="32" x="96" y="32" fill="grey" />
-  <rect width="32" height="32" x="0" y="64" fill="blue" />
-  <rect width="32" height="32" x="32" y="64" fill="cyan" />
-  <rect width="32" height="32" x="64" y="64" fill="pink" />
-  <rect width="32" height="32" x="96" y="64" fill="red" />
-  <rect width="32" height="32" x="0" y="96" fill="rose" />
-  <rect width="32" height="32" x="32" y="96" fill="purple" />
-  <rect width="32" height="32" x="64" y="96" fill="orange" />
-  <rect width="32" height="32" x="96" y="96" fill="yellow" />
-  Sorry, your browser does not support inline SVG.  
-</svg>
-	);
 
 	return (
     <>
@@ -79,8 +54,7 @@ const onChangeSize = (newSize) => {
         </PanelBody>
       </InspectorControls>
       <div {...blockProps}>
-        Here
-        {svg}
+        Pixel Art Drawing Placeholder
       </div>
     </>
 	);
